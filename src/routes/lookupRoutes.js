bcb6e46926cd22eaf6ee1f0a1f34a802697016fc
rachidnejiuae-del <rachidneji.uae@gmail.com>
2026-07-01 11:45:46 +0300

    Update lookupRoutes.js

--- src/routes/lookupRoutes.js
+++ src/routes/lookupRoutes.js
@@ -1,115 +1,37 @@
-// Complete official delegations (délégations) of Tunisia per governorate.
-// Source: Wikipedia / Ministry of Interior Tunisia (2024 data, 279 delegations).
-module.exports = {
-  'Tunis': [
-    'Bab El Bhar', 'Bab Souika', 'Carthage', 'Cité El Khadra', 'Djebel Jelloud',
-    'El Hraïria', 'El Kabaria', 'El Menzah', 'El Omrane', 'El Omrane Supérieur',
-    'El Ouardia', 'Ettahrir', 'Ezzouhour', 'La Goulette', 'La Marsa',
-    'Le Bardo', 'Le Kram', 'Medina', 'Séjoumi', 'Sidi El Béchir', 'Sidi Hassine',
-  ],
-  'Ariana': [
-    'Ariana Ville', 'Cité Ettadhamen', 'Kalâat El Andalous',
-    'La Soukra', 'Mnihla', 'Raoued', 'Sidi Thabet',
-  ],
-  'Ben Arous': [
-    'Ben Arous', 'Bou Mhel El Bassatine', 'El Mourouj', 'Ezzahra', 'Fouchana',
-    'Hammam Chott', 'Hammam Lif', 'Medina Jedida', 'Mégrine',
-    'Mohamedia', 'Mornag', 'Radès',
-  ],
-  'Manouba': [
-    'Borj El Amri', 'Djedeida', 'Douar Hicher', 'El Batan',
-    'Manouba', 'Mornaguia', 'Oued Ellil', 'Tebourba',
-  ],
-  'Nabeul': [
-    'Béni Khalled', 'Béni Khiar', 'Bou Argoub', 'Dar Châabane El Fehri',
-    'El Haouaria', 'El Mida', 'Grombalia', 'Hammamet', 'Hammam Ghezèze',
-    'Kélibia', 'Korba', 'Menzel Bouzelfa', 'Menzel Temime', 'Nabeul',
-    'Soliman', 'Takelsa',
-  ],
-  'Zaghouan': [
-    'Bir Mcherga', 'El Fahs', 'Nadhour', 'Saouaf', 'Zaghouan', 'Zriba',
-  ],
-  'Bizerte': [
-    'Bizerte Nord', 'Bizerte Sud', 'El Alia', 'Ghar El Melh', 'Ghezala',
-    'Joumine', 'Mateur', 'Menzel Bourguiba', 'Menzel Jemil', 'Ras Jebel',
-    'Sejnane', 'Tinja', 'Utique', 'Zarzouna',
-  ],
-  'Béja': [
-    'Amdoun', 'Béja Nord', 'Béja Sud', 'Goubellat', 'Medjez El Bab',
-    'Nefza', 'Téboursouk', 'Testour', 'Thibar',
-  ],
-  'Jendouba': [
-    'Aïn Draham', 'Balta - Bou Aouane', 'Bou Salem', 'Fernana',
-    'Ghardimaou', 'Jendouba', 'Jendouba Nord', 'Oued Meliz', 'Tabarka',
-  ],
-  'Le Kef': [
-    'Dahmani', 'El Ksour', 'Jérissa', 'Kalâat Khasba', 'Kalaat Senan',
-    'Kef Est', 'Kef Ouest', 'Nebeur', 'Sakiet Sidi Youssef',
-    'Sers', 'Tajerouine', 'Touiref',
-  ],
-  'Siliana': [
-    'Bargou', 'Bou Arada', 'El Aroussa', 'El Krib', 'Gaâfour',
-    'Kesra', 'Makthar', 'Rouhia', 'Sidi Bou Rouis', 'Siliana Nord', 'Siliana Sud',
-  ],
-  'Sousse': [
-    'Akouda', 'Bouficha', 'Enfida', 'Hammam Sousse', 'Hergla',
-    'Kalâa Kebira', 'Kalâa Seghira', 'Kondar', 'M\'saken', 'Sidi Bou Ali',
-    'Sidi El Hani', 'Sousse Jawhara', 'Sousse Médina', 'Sousse Riadh',
-    'Sousse Sidi Abdelhamid', 'Zaouiet - Ksibet Thrayet',
-  ],
-  'Monastir': [
-    'Bekalta', 'Bembla', 'Beni Hassen', 'Jemmal', 'Ksar Hellal',
-    'Ksibet El Médiouni', 'Moknine', 'Monastir', 'Ouerdanine',
-    'Sahline', 'Sayada - Lamta - Bouhjar', 'Téboulba', 'Zéramdine',
-  ],
-  'Mahdia': [
-    'Bou Merdes', 'Chebba', 'Chorbane', 'El Bradâa', 'El Jem',
-    'Essouassi', 'Hebira', 'Ksour Essef', 'Mahdia', 'Melloulèche',
-    'Ouled Chamekh', 'Rejiche', 'Sidi Alouane',
-  ],
-  'Sfax': [
-    'Agareb', 'Bir Ali Ben Khalifa', 'El Amra', 'El Hencha', 'Graïba',
-    'Jebiniana', 'Kerkennah', 'Mahrès', 'Menzel Chaker', 'Sakiet Eddaïer',
-    'Sakiet Ezzit', 'Sfax Ouest', 'Sfax Sud', 'Sfax Ville', 'Skhira', 'Thyna',
-  ],
-  'Kairouan': [
-    'Aïn Djeloula', 'Bou Hajla', 'Chebika', 'Echrarda', 'El Alâa',
-    'Haffouz', 'Hajeb el Ayoun', 'Kairouan Nord', 'Kairouan Sud',
-    'Menzel Mehiri', 'Nasrallah', 'Oueslatia', 'Sbikha',
-  ],
-  'Kasserine': [
-    'El Ayoun', 'Ezzouhour', 'Fériana', 'Foussana', 'Haïdra',
-    'Hassi El Ferid', 'Jedelienne', 'Kasserine Nord', 'Kasserine Sud',
-    'Majel Bel Abbès', 'Sbeïtla', 'Sbiba', 'Thala',
-  ],
-  'Sidi Bouzid': [
-    'Bir El Hafey', 'Cebbala Ouled Asker', 'El Hichria', 'Essaïda',
-    'Jilma', 'Meknassy', 'Menzel Bouzaiane', 'Mezzouna', 'Ouled Haffouz',
-    'Regueb', 'Sidi Ali Ben Aoun', 'Sidi Bouzid Est', 'Sidi Bouzid Ouest', 'Souk Jedid',
-  ],
-  'Gabès': [
-    'Dkhilet Toujane', 'El Hamma', 'Gabès Médina', 'Gabès Ouest', 'Gabès Sud',
-    'Ghannouch', 'Habib Thameur Bouatouch', 'Mareth', 'Matmata',
-    'Menzel El Habib', 'Métouia', 'Nouvelle Matmata', 'Oudhref',
-  ],
-  'Médenine': [
-    'Ben Gardane', 'Beni Khedache', 'Djerba Ajim', 'Djerba Houmt Souk',
-    'Djerba Midoun', 'Médenine Nord', 'Médenine Sud', 'Sidi Makhlouf', 'Zarzis',
-  ],
-  'Tataouine': [
-    'Beni Mehira', 'Bir Lahmar', 'Dehiba', 'Ghomrassen',
-    'Remada', 'Smâr', 'Tataouine Nord', 'Tataouine Sud',
-  ],
-  'Gafsa': [
-    'Belkhir', 'El Guettar', 'El Ksar', 'Gafsa Nord', 'Gafsa Sud',
-    'Mdhila', 'Métlaoui', 'Moularès', 'Redeyef', 'Sened',
-    'Sidi Aïch', 'Sidi Boubaker', 'Zannouch',
-  ],
-  'Tozeur': [
-    'Degache', 'El Hamma du Jérid', 'Hazoua', 'Nefta', 'Tamerza', 'Tozeur',
-  ],
-  'Kébili': [
-    'Douz Nord', 'Douz Sud', 'Faouar', 'Kébili Nord', 'Kébili Sud',
-    'Rjim Maatoug', 'Souk Lahad',
-  ],
-};
+const express = require('express');
+const pool = require('../db/pool');
+const AREAS_BY_GOVERNORATE = require('../data/areas');
+
+const router = express.Router();
+
+const GOVERNORATES = [
+  'Tunis', 'Ariana', 'Ben Arous', 'Manouba', 'Nabeul', 'Zaghouan', 'Bizerte',
+  'Béja', 'Jendouba', 'Le Kef', 'Siliana', 'Sousse', 'Monastir', 'Mahdia',
+  'Sfax', 'Kairouan', 'Kasserine', 'Sidi Bouzid', 'Gabès', 'Médenine',
+  'Tataouine', 'Gafsa', 'Tozeur', 'Kébili',
+];
+
+router.get('/subjects', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT id, name FROM subjects ORDER BY name');
+    res.json({ subjects: result.rows });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erreur serveur.' });
+  }
+});
+
+router.get('/governorates', (req, res) => {
+  res.json({ governorates: GOVERNORATES });
+});
+
+router.get('/areas', (req, res) => {
+  const { governorate } = req.query;
+  if (!governorate) {
+    return res.status(400).json({ error: 'Le paramètre governorate est requis.' });
+  }
+  const areas = AREAS_BY_GOVERNORATE[governorate] || [];
+  res.json({ areas });
+});
+
+module.exports = router;
