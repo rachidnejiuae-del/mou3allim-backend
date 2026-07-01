f3400724a6c2ee6d378a95cc5e6628fc24ebfa07
rachidnejiuae-del <rachidneji.uae@gmail.com>
2026-07-01 11:22:09 +0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login, bootstrapAdmin } = require('../controllers/authController');
+const { sendOtp, verifyOtp } = require('../controllers/otpController');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/bootstrap-admin', bootstrapAdmin);
+router.post('/send-otp', sendOtp);
+router.post('/verify-otp', verifyOtp);
 
 module.exports = router;
