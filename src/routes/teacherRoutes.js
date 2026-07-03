65404706879a1acfcc3fce3f6efa9315d9e678a0
rachidnejiuae-del <rachidneji.uae@gmail.com>
2026-07-04 00:54:42 +0300

    Update teacherRoutes.js

--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -4,20 +4,17 @@ const upload = require('../middleware/upload');
 const {
   updateMyProfile, uploadPhoto, uploadCertificate, search, getById,
 } = require('../controllers/teacherController');
-const { rate, list: listRatings } = require('../controllers/ratingController');
+const { rate, list: listRatings, hide, unhide } = require('../controllers/ratingController');
 
 const router = express.Router();
 
-// Public
 router.get('/search', search);
 router.get('/:id', getById);
 router.get('/:id/ratings', listRatings);
+router.post('/:id/ratings', rate);
 
-// Authenticated (teacher)
 router.put('/me', authenticate, requireRole('teacher'), updateMyProfile);
 router.post('/me/photo', authenticate, requireRole('teacher'), upload.single('photo'), uploadPhoto);
 router.post('/me/certificate', authenticate, requireRole('teacher'), upload.single('certificate'), uploadCertificate);
 
-// Authenticated (parent)
-router.post('/:id/ratings', rate);
 module.exports = router;
